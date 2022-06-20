d0e7f5c4c43104c4774b13e3631af824e59cd19e
klukvenka <aida_g888@mail.ru>
2022-06-20 22:52:00 +0200

    Personal info and Address check

--- 5-APIs/worker.js
+++ 5-APIs/worker.js
@@ -18,7 +18,7 @@ const config = {
 // create a Client instance with custom configuration
 const client = new Client(config);
 
-// susbscribe to the topic: 'charge-card'
+// Control type of medicine
 client.subscribe("control-medicine", async function ({ task, taskService }) {
   const process_vars = new Variables();
   const prescription = task.variables.get("prescription");
@@ -33,11 +33,13 @@ client.subscribe("control-medicine", async function ({ task, taskService }) {
   await taskService.complete(task, process_vars);
 });
 
+// Contact Ministry of Health
 client.subscribe("ministry-health", async function ({ task, taskService }) {
   console.log(`PRESCRIPTION SENT TO MINISTRY OF HEALTH`);
   await taskService.complete(task);
 });
 
+// Validate Prescription
 client.subscribe("prescription-validation", async function ({ task, taskService }) {
     const process_vars = new Variables();
     const prescription = task.variables.get("prescription");
@@ -50,4 +52,41 @@ client.subscribe("prescription-validation", async function ({ task, taskService 
   
     console.log(`PRESCRIPTION VALIDATED`);
     await taskService.complete(task, process_vars);
+  });
+
+// Validate Information
+client.subscribe("validate-information", async function ({ task, taskService }) {
+    const process_vars = new Variables();
+    const name = task.variables.get("name");
+    const tax_code = task.variables.get("tax-code");
+  
+    if (name && tax_code) {
+      process_vars.set("personal_info_valid", "true");
+    } else {
+      process_vars.set("personal_info_valid", "false");
+    }
+  
+    console.log(`PERSONAL INFORMATION CHECKED`);
+    await taskService.complete(task, process_vars);
+  });
+
+// Contact Google Maps API
+client.subscribe("google-api", async function ({ task, taskService }) {
+    console.log(`PRESCRIPTION SENT TO MINISTRY OF HEALTH`);
+    await taskService.complete(task);
+  });
+
+// Check Delivery address
+client.subscribe("check-address", async function ({ task, taskService }) {
+    const process_vars = new Variables();
+    const address = task.variables.get("address");
+
+    if (address) {
+        process_vars.set("address_valid", "true");
+      } else {
+        process_vars.set("address_valid", "false");
+      }
+
+    console.log(`ADDRESS CHECKED`);
+    await taskService.complete(task,process_vars);
   });
